dcbd03af4e051309725b150fc940d4d29810acf5
PierreFch <pierrelefaucheur252@gmail.com>
2022-02-01 10:22:04 +0100

    Création des classes ToDo et ToDoList

--- todo.php
+++ todo.php
@@ -1,1 +1,42 @@
 <?php
+
+class ToDo
+{
+  public function __construct(public string $title, public ?string $descritpion, public ?DateTime $done_at)
+  {
+  }
+  public function setDone(): self
+  {
+    $this->done_at = new DateTime();
+    return $this;
+  }
+
+  public function unDone(): self
+  {
+    unset($this->done_at);
+    return $this;
+  }
+}
+
+class ToDoList
+{
+  public function __construct(public array $todos)
+  {
+  }
+  public function showDone(): array
+  {
+
+  }
+
+  public function showToDo(): array
+  {
+
+  }
+
+  public function setAllDone(): self
+  {
+    
+  }
+}
+
+$todolist = new ToDoList();
