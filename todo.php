a9b159700d9ed542d0d5d7fd8e5406e71fa0eb92
PierreFch <pierrelefaucheur252@gmail.com>
2022-02-01 13:44:09 +0100

    Ajout des methods dans la class ToDoList

--- todo.php
+++ todo.php
@@ -21,12 +21,19 @@ class Todo
 
 class ToDoList
 {
-  public array $todos;
+  public array $todos = [];
+
+  // Ajouter une todo
+  public function addTodo(Todo $todo): self
+  {
+    $this->todos[] = $todo; // Comme array_push mais plus performant
+    return $this;
+  }
 
   // Afficher les todos terminées
   public function showCompleted(): array
   {
-    array_filter($this->todos, function(Todo $todo){
+    array_filter($this->todos, function (Todo $todo) {
       return $todo->isCompleted();
     });
   }
@@ -34,7 +41,7 @@ class ToDoList
   // Afficher les todos en cours
   public function showNotCompleted(): array
   {
-    array_filter($this->todos, function(Todo $todo){
+    array_filter($this->todos, function (Todo $todo) {
       return $todo->unCompleted();
     });
   }
@@ -42,16 +49,16 @@ class ToDoList
   // Valider toutes les todos
   public function setAllCompleted(): self
   {
-    foreach($this->todos as $todo){
+    foreach ($this->todos as $todo) {
       return $todo === true;
     }
   }
-
-  // Ajouter une todo
-  public function addTodo(array $todo): self
-  {
-    $this->todos[]=$todo; // Comme array_push mais plus performant
-  }
 }
 
-$todos = [];
+$list = new ToDoList();
+$list
+  ->addTodo(new Todo("Ma tâche numéro 1", "01-02-2022"))
+  ->addTodo(new Todo("Ma tâche numéro 2", "10-02-2022"))
+  ->addTodo(new Todo("Ma tâche numéro 3", "25-02-2022"));
+
+var_dump($list);
