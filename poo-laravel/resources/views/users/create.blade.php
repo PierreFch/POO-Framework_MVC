faae0821a6d8c029379b98c9c39ac9b570a0a67b
PierreFch <pierrelefaucheur252@gmail.com>
2022-03-11 14:05:31 +0100

    Reset password with Laravel

--- poo-laravel/resources/views/users/create.blade.php
+++ poo-laravel/resources/views/users/create.blade.php
@@ -9,38 +9,47 @@
         <div class="form-row">
             <div class="text my-3 col">
                 <label for="name">Nom :</label>
-                <input type="text" id="name" name="name" value="{{ old('name') }}" class="form-control @error('name') is-invalid @enderror">
+                <input type="text" id="name" name="name" value="{{ old('name') }}"
+                       class="form-control @error('name') is-invalid @enderror">
                 @error('name')
-                    <div class="invalid-feedback">{{ $message }}</div>
+                <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
             <div class="text my-3 col">
                 <label for="email">Email :</label>
-                <input type="email" id="email" name="email" value="{{ old('email') }}" class="form-control @error('email') is-invalid @enderror">
+                <input type="email" id="email" name="email" value="{{ old('email') }}"
+                       class="form-control @error('email') is-invalid @enderror">
                 @error('email')
-                    <div class="invalid-feedback">{{ $message }}</div>
+                <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
         </div>
         <div class="form-row">
             <div class="text my-3 col">
                 <label for="password">Mot de passe :</label>
-                <input type="password" id="password" name="password" value="{{ old('password') }}" class="form-control @error('password') is-invalid @enderror">
+                <input type="password" id="password" name="password" value="{{ old('password') }}"
+                       class="form-control @error('password') is-invalid @enderror">
                 @error('password')
-                    <div class="invalid-feedback">{{ $message }}</div>
+                <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
             <div class="text my-3 col">
-                <input type="password" placeholder="Confirm password" id="password_confirmation" name="password_confirmation" class="form-control @error ('password-confirmation')is-invalid @enderror">
+                <label for="password_confirmation">Confirmer mot de passe :</label>
+                <input type="password" id="password_confirmation" name="password_confirmation"
+                       class="form-control @error ('password-confirmation')is-invalid @enderror">
                 @error ('password_confirmation')
                 <div class="invalid-feedback"> {{ $message }}</div>
                 @enderror
             </div>
+        </div>
+        <div class="form-row">
             <div class="text my-3 col">
                 <label for="avatar_url">Photo de profil :</label>
-                <input type="text" id="avatar_url" name="avatar_url" value="https://fakeimg.pl//200x200/282828/eae0d0/?retina=1" class="form-control @error('avatar_url') is-invalid @enderror">
+                <input type="text" id="avatar_url" name="avatar_url"
+                       value="https://fakeimg.pl//200x200/282828/eae0d0/?retina=1"
+                       class="form-control @error('avatar_url') is-invalid @enderror">
                 @error('avatar_url')
-                    <div class="invalid-feedback">{{ $message }}</div>
+                <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
         </div>
--- poo-laravel/resources/views/users/edit.blade.php
+++ poo-laravel/resources/views/users/edit.blade.php
@@ -12,38 +12,48 @@
             <div class="form-row">
                 <div class="text my-3 col">
                     <label for="name">Nom :</label>
-                    <input type="text" id="name" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ $user->name }}">
+                    <input type="text" id="name" name="name" class="form-control @error('name') is-invalid @enderror"
+                           value="{{ $user->name }}">
                     @error('name')
-                        <div class="invalid-feedback">{{ $message }}</div>
+                    <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="text my-3 col">
                     <label for="email">Email :</label>
-                    <input type="email" id="email" name="email" class="form-control @error('email') is-invalid @enderror" value="{{ $user->email }}">
+                    <input type="email" id="email" name="email"
+                           class="form-control @error('email') is-invalid @enderror" value="{{ $user->email }}">
                     @error('email')
-                        <div class="invalid-feedback">{{ $message }}</div>
+                    <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
             </div>
             <div class="form-row">
                 <div class="text my-3 col">
                     <label for="password">Mot de passe :</label>
-                    <input type="password" id="password" name="password" class="form-control @error('password') is-invalid @enderror" value="{{ $user->password }}">
+                    <input type="password" id="password" name="password"
+                           class="form-control @error('password') is-invalid @enderror" value="{{ $user->password }}">
                     @error('password')
-                        <div class="invalid-feedback">{{ $message }}</div>
+                    <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="text my-3 col">
-                    <input type="password" placeholder="Confirm password" id="password_confirmation" name="password_confirmation" class="form-control @error ('password-confirmation')is-invalid @enderror">
+                    <label for="password_confirmation">Confirmer mot de passe :</label>
+                    <input type="password" id="password_confirmation"
+                           name="password_confirmation" value="{{ $user->password }}"
+                           class="form-control @error ('password-confirmation')is-invalid @enderror">
                     @error ('password_confirmation')
                     <div class="invalid-feedback"> {{ $message }}</div>
                     @enderror
                 </div>
+            </div>
+            <div class="form-row">
                 <div class="text my-3 col">
                     <label for="avatar_url">Photo de profil :</label>
-                    <input type="text" id="avatar_url" name="avatar_url" class="form-control @error('avatar_url') is-invalid @enderror" value="{{ $user->avatar_url }}">
+                    <input type="text" id="avatar_url" name="avatar_url"
+                           class="form-control @error('avatar_url') is-invalid @enderror"
+                           value="{{ $user->avatar_url }}">
                     @error('avatar_url')
-                        <div class="invalid-feedback">{{ $message }}</div>
+                    <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
             </div>
