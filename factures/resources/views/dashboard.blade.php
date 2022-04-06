a017c835eb48324d9acd303f657c88be8ac1f8b3
PierreFch <pierrelefaucheur252@gmail.com>
2022-04-06 17:32:04 +0200

    Intégration dashboard

--- factures/resources/views/dashboard.blade.php
+++ factures/resources/views/dashboard.blade.php
@@ -1,1 +1,47 @@
 @extends('layout')
+
+@section('content')
+    <div class="divPage divDashboard">
+        <div class="left">
+            <h2>Choisissez un modèle :</h2>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="" title="Devis">Devis</a>
+                    </li>
+                    <li>
+                        <a href="" title="Facture accompte">Facture accompte</a>
+                    </li>
+                    <li>
+                        <a href="" title="Facture sans devis">Facture sans devis</a>
+                    </li>
+                    <li>
+                        <a href="" title="Facture solde">Facture solde</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <div class="right">
+            <h1>Mes documents</h1>
+            <section class="devis">
+                <h2>Devis</h2>
+
+            </section>
+
+            <section class="facture-accompte">
+                <h2>Facture accompte</h2>
+
+            </section>
+
+            <section class="facture-sans-devis">
+                <h2>Facture sans devis</h2>
+
+            </section>
+
+            <section class="facture-solde">
+                <h2>Facture solde</h2>
+
+            </section>
+        </div>
+    </div>
+@endsection
